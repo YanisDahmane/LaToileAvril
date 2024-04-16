494378f66b0e3507018c5e1d64d8d02cf1b56518
yanis dahmane <yanis.dahmane.pro@gmail.com>
2024-04-16 16:43:02 +0200

    c

--- 16-avril/correction/alea/index.js
+++ 16-avril/correction/alea/index.js
@@ -1,6 +1,6 @@
 console.log("ok")
 const aleatoire = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1)) + min
+  return Math.round(Math.random() * (max - min)) + min
 }
 
 console.log(aleatoire(5, 8))
--- 16-avril/correction/palindrome/index.js
+++ 16-avril/correction/palindrome/index.js
@@ -15,25 +15,34 @@
 
 // const wordReverse = "roujnob"
 
-const checkPalindrome = (word) => {
-  // On mets notre mot en majuscule
-  word = word.toUpperCase()
-  // On créé un tableau composé de toute nos lettres en majuscule
-  const array = word.split("")
-  // On inverse notre tableau le premier élément se retrouve à la fin ect.
-  const arrayReverse = array.reverse()
-  // De notre tableau inversé on la remet sous forme de string
-  const reverseWord = arrayReverse.join("")
-  // On test si notre mot de départ est identique à notre mot inversé
-  return reverseWord === word
+// const checkPalindrome = (word) => {
+//   // On mets notre mot en majuscule
+//   word = word.toUpperCase()
+//   // On créé un tableau composé de toute nos lettres en majuscule
+//   const array = word.split("")
+//   // On inverse notre tableau le premier élément se retrouve à la fin ect.
+//   const arrayReverse = array.reverse()
+//   // De notre tableau inversé on la remet sous forme de string
+//   const reverseWord = arrayReverse.join("")
+//   // On test si notre mot de départ est identique à notre mot inversé
+//   return reverseWord === word
 
 
-  // return word.toUpperCase().split("").reverse().join("") === word.toUpperCase()
-}
+//   // return word.toUpperCase().split("").reverse().join("") === word.toUpperCase()
+// }
 
 
 
-console.log("kayak", checkPalindrome("kayak"))
-console.log("sandy", checkPalindrome("sandy"))
+// console.log("kayak", checkPalindrome("kayak"))
+// console.log("sandy", checkPalindrome("sandy"))
 
-console.log("bonJour".toUpperCase().split("").reverse())
+// console.log(["a", "b", "c"].join(""))
+
+
+let firstName = "Yanis"
+
+console.log(firstName)
+
+firstName = "Sandy"
+
+console.log(firstName)
