a533e2a564b6a329c4b25c58d5d6ac6af707520e
yanis dahmane <yanis.dahmane.pro@gmail.com>
2024-04-16 16:17:59 +0200

    kk

---
+++ 16-avril/correction/palindrome/index.js
@@ -0,0 +1,39 @@
+// console.log("Bonjour")
+// let wordTest = "bonjour"
+// wordTest = wordTest.toUpperCase();
+// const array = wordTest.split("")
+// // array => ['b', 'o', 'n', 'j', 'o', 'u', 'r']
+// console.log(array)
+// const arrayReverse = array.reverse()
+// // arrayReverse => ['r', 'u', 'o', 'j', 'n', 'o', 'b']
+// console.log(arrayReverse)
+// const reverseWord = arrayReverse.join("")
+// // reverseWord = ruojnob
+// console.log(reverseWord)
+
+// console.log("verif: ", reverseWord == wordTest)
+
+// const wordReverse = "roujnob"
+
+const checkPalindrome = (word) => {
+  // On mets notre mot en majuscule
+  word = word.toUpperCase()
+  // On créé un tableau composé de toute nos lettres en majuscule
+  const array = word.split("")
+  // On inverse notre tableau le premier élément se retrouve à la fin ect.
+  const arrayReverse = array.reverse()
+  // De notre tableau inversé on la remet sous forme de string
+  const reverseWord = arrayReverse.join("")
+  // On test si notre mot de départ est identique à notre mot inversé
+  return reverseWord === word
+
+
+  // return word.toUpperCase().split("").reverse().join("") === word.toUpperCase()
+}
+
+
+
+console.log("kayak", checkPalindrome("kayak"))
+console.log("sandy", checkPalindrome("sandy"))
+
+console.log("bonJour".toUpperCase().split("").reverse())
