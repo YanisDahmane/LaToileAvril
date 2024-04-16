04ba8c982e7fc78627e185ed3cca8ab00bc82fa4
yanis dahmane <yanis.dahmane.pro@gmail.com>
2024-04-16 14:39:04 +0200

    nn

---
+++ 16-avril/correction/pair/index.js
@@ -0,0 +1,17 @@
+// const number = 49
+
+const estPaire = (number) => {
+  // if (number % 2 === 0) {
+  //   // Nombre paire
+  //   return true
+  // } else {
+  //   // Nombre impaire
+  //   return false
+  // }
+
+  return number % 2 === 0
+}
+
+
+console.log("paire", estPaire(68))
+console.log("impaire", estPaire(79))
--- 16-avril/cours/fonction/index.js
+++ 16-avril/cours/fonction/index.js
@@ -1,25 +1,44 @@
+// const normalize = (string) => {
+//   const firstLetter = string[0].toUpperCase()
+//   const otherLetter = string.substring(1).toLowerCase()
+//   return `${firstLetter}${otherLetter}`
+// }
+
+// const fullName = (prenom, nom) => {
+//   return `${normalize(prenom)} ${normalize(nom)}`
+// }
+
+// // const addition = (a, b) => {
+// //   return a + b
+// // }
+
+// // console.log(addition(2, 3))
+
+// console.log(fullName("yanis", "dahmane"))
+
+// console.log(fullName("sandy", "blue"))
+
+// console.log(fullName("joE", "dOE"))
+
+// console.log(firstLetter)
+
+// // console.log(normalize("yAniS"))
+
+// const addition = (a, b) => {
+//   return a + b
+// }
+// console.log(addition(2, 5)) // => 7
+
 const normalize = (string) => {
-  const firstLetter = string[0].toUpperCase()
-  const otherLetter = string.substring(1).toLowerCase()
-  return `${firstLetter}${otherLetter}`
+  return `${string[0].toUpperCase()}${string.substring(1).toLowerCase()}`
 }
 
 const fullName = (prenom, nom) => {
   return `${normalize(prenom)} ${normalize(nom)}`
 }
 
-// const addition = (a, b) => {
-//   return a + b
-// }
+console.log(fullName("yaNis", "dahmane")) // => "yanis dahmane"
+console.log(fullName("sandy", "blUE"))
 
-// console.log(addition(2, 3))
 
-console.log(fullName("yanis", "dahmane"))
-
-console.log(fullName("sandy", "blue"))
-
-console.log(fullName("joE", "dOE"))
-
-console.log(firstLetter)
-
-// console.log(normalize("yAniS"))
+console.log(estPaire(42)) // => true
