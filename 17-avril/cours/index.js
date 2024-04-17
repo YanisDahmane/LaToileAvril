bee58cd04843f567577aec00a42a927d5ac5c554
yanis dahmane <yanis.dahmane.pro@gmail.com>
2024-04-17 09:56:37 +0200

    cours

---
+++ 17-avril/cours/index.js
@@ -0,0 +1,73 @@
+// // let fruits = ["Pomme", "Banane", "Orange", "Fraise"];
+// // console.log("fruits", fruits)
+
+// // console.log(fruits[1])
+// // fruits[1] = "pêche"
+// // console.log(fruits[1])
+
+// // console.log("length", fruits.length)
+
+// // console.log(fruits[fruits.length - 1])
+
+// // fruits.push("Poire")
+
+// // console.log(fruits)
+
+// // fruits.pop()
+
+// // console.log(fruits)
+
+// // fruits.shift()
+
+// // console.log(fruits)
+
+// // fruits.unshift("Pomme")
+
+// // console.log(fruits)
+
+// // console.log(fruits.indexOf("Orange"))
+
+// // let numbers = [10, 5, 8, 2, 1];
+
+// // console.log(numbers)
+
+// // console.log(numbers.sort())
+
+// // console.log(fruits.sort())
+
+// // let elementFiltre = numbers.filter((chiffre) => {
+// //   return chiffre > 5
+// // })
+
+// // let first5 = numbers.find((chiffre) => {
+// //   return chiffre > 5
+// // })
+
+// // console.log(elementFiltre)
+// // console.log(first5)
+
+// let prenoms = ['Alice', 'Bob', 'Charlie', 'David', 'Yanis'];
+
+// prenoms = prenoms.filter((prenom) => {
+//   return prenom !== 'Bob'
+// }) // =>['Alice', 'Charlie', 'David', 'Yanis'];
+
+// console.log(prenoms)
+
+// // for (let index = 0; index < prenoms.length; index++) {
+// //   console.log("index", index)
+// //   console.log("Bonjour", prenoms[index])
+// // }
+
+// prenoms.forEach((prenom, index) => {
+//   console.log("Bonjour", prenom)
+//   console.log("index", index)
+//   prenom = 'Yanis'
+// })
+let i = 0
+
+do {
+  i++
+  console.log('i', i)
+}
+while (false)
