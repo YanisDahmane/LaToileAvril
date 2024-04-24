726d41f57f2f13c3b875f715a9db9459f13f75eb
yanis dahmane <yanis.dahmane.pro@gmail.com>
2024-04-24 12:29:25 +0200

    avancé

--- 24-avril/template/assets/js/script.js
+++ 24-avril/template/assets/js/script.js
@@ -1,1 +1,18 @@
 console.log("Bonjour 😀")
+const form = document.querySelector("#formulaire")
+console.log(form)
+const namedom = document.querySelector("#name")
+console.log(namedom)
+const descriptiondom = document.querySelector("#description")
+console.log(descriptiondom)
+const imagedom = document.querySelector("#image")
+console.log(imagedom)
+form.addEventListener("submit", (event) => {
+  event.preventDefault()
+  const name = namedom.value
+  console.log("name", name)
+  const description = descriptiondom.value
+  console.log("description", description)
+  const image = imagedom.value
+  console.log("image", image)
+})
